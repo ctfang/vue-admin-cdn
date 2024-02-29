5ebce3add50380413a71d6665ab7e77fae985008
yuanzhao <2206582181@qq.com>
2024-02-29 21:04:20 +0800

    add: test

--- static/importmap.js
+++ static/importmap.js
@@ -2,7 +2,7 @@ const im = document.createElement("script");
 im.type = "importmap";
 im.textContent = JSON.stringify({
   imports: {
-    "@/": "/",
+    "@/": "./",
     vue: "/modules/vue.js",
     "element-plus": "/modules/element-plus.js",
     "vue-router": "https://unpkg.com/vue-router@4.0.15/dist/vue-router.esm-browser.js",
