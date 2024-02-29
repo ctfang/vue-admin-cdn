3c19f60caaf142fd7490364ab2a21156e2957fdb
yuanzhao <2206582181@qq.com>
2024-02-29 21:12:16 +0800

    add: test

--- static/importmap.js
+++ static/importmap.js
@@ -3,8 +3,8 @@ im.type = "importmap";
 im.textContent = JSON.stringify({
   imports: {
     "@/": "./",
-    vue: "/modules/vue.js",
-    "element-plus": "/modules/element-plus.js",
+    vue: "./modules/vue.js",
+    "element-plus": "./modules/element-plus.js",
     "vue-router": "https://unpkg.com/vue-router@4.0.15/dist/vue-router.esm-browser.js",
     "@vue/devtools-api": "https://unpkg.com/@vue/devtools-api@6.6.1/lib/esm/index.js",
   },
